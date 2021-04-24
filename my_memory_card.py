65ca1b685948c2d07b3308d470d56eb0881c5e80
logikaschool <82713228+logikaschool@users.noreply.github.com>
2021-04-24 16:50:09 +0300

    prototype of the game

--- my_memory_card.py
+++ my_memory_card.py
@@ -20,13 +20,43 @@ class Player(GameSprite):
             keys = key.get_pressed()
             if keys[K_UP] and self.rect.y > 5:
                 self.rect.y -= self.speed
-            if keys[K_DOWN] and self.rect.y < win_height - 80:
+            if keys[K_DOWN] and self.rect.y < win_height - 45:
                 self.rect.y += self.speed
         def update_l(self):
             keys = key.get_pressed()
             if keys[K_w] and self.rect.y > 5:
                 self.rect.y -= self.speed
-            if keys[K_s] and self.rect.y < win_height - 80:
+            if keys[K_s] and self.rect.y < win_height - 45:
                 self.rect.y += self.speed
 
+back = (200,255,255)
+win_width = 600
+win_height = 500
 
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 120
+
+racket1 = Player('ping1.jpg', 30, 200, 60, 60, 5)
+racket2 = Player('ping2.jpg', 520, 200, 60, 60, 5)
+ball = GameSprite('ball.jpg', 200,200, 30,30, 50)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+    
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+    display.update()
+    clock.tick(FPS)
